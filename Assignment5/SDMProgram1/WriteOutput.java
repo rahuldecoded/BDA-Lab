bc4d2515bd1a5b9474b5eaf88a6da1115898cdcd
Rahul Bhattacharjee <2806rahul@gmail.com>
2020-11-21 01:02:48 +0530

    Add files via upload

---
+++ Assignment5/SDMProgram1/WriteOutput.java
@@ -0,0 +1,48 @@
+package SDMPackage1;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteOutput {
+	// Function To Insert a clean line in a File
+	// Clean Line: Line without stop words
+	static String outputFile = "C:\\hadoop\\lab_data\\SDM-Data\\output1.txt";
+	
+	public static void insertToFile(String args) {
+		File fileObj = null;
+		try {
+			fileObj = new File(outputFile);
+			if (fileObj.createNewFile()) {
+				System.out.println("File created");
+		    }
+		}
+		catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+			
+		BufferedWriter bw = null;
+		try {
+
+			bw = new BufferedWriter(new FileWriter(fileObj, true));
+			bw.write(args + "\n");
+				
+				
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		} finally {
+			try {
+				if (bw != null) {
+					bw.close();
+				}
+			} 
+			catch (IOException e) {
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			};
+		}
+	}	
+}
